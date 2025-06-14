44f4b89bf4ed5923627a8e9925987d085b464fbd
chrisngili <ngilichristopher@gmail.com>
2025-06-14 21:29:45 +0300

    models definition complete, migrations commence

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from models import db, User, Restaurant, Reservation, Review, Menu
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
 
 
 
--- models.py
+++ models.py
@@ -17,14 +17,48 @@ class User(db.Model):
     phone = db.Column(db.Integer, nullable=False, unique=True)
     is_admin = db.Column(db.Boolean, default=False)
 
+    reservations = db.relationship('Reservation', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    # reviews = db.relationship('Review', backref='user', lazy=True) for restaurnats
+
 class Restaurant(db.Model):
     __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    location = db.Column(db.String, nullable=False, unique=True)
+    contacts = db.Column(db.Integer, nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('users.admin.id'), nullable=False)
+
+    reservations = db.relationship('Reservation', backref='restaurant', lazy=True)
+    menus = db.relationship('Menu', backref='restaurant', lazy=True)
+    reviews = db.relationship('Review', backref='restaurant', lazy=True)
 
 class Reservation(db.Model):
     __tablename__ = 'reservations'
 
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.timezone.utcnow)
+    party_size = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeginKey('restaurants.id'), nullable=False)
+
 class Menu(db.Model):
     __tablename__ = 'menus'
 
+    id = db.Column(db.Integer, primary_key=True)
+    item-name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeginKey('restaurants.id'), nullable=False)
+
 class Review(db.Model):
     __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    date = db.Column(db.DateTime, default=datetime.timezone.utcnow)
+    restaurant_id = db.Column(db.Integer, db.ForeginKey('restaurants.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
